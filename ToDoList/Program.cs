9e77a66bfffecf385e9e4db13b006622c96f538d
LSLIM <LSLIM@LS>
2018-04-07 12:44:33 +0200

    I add some classes and methods in the application

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -11,10 +11,39 @@ namespace ToDoList
     Maak een applicatie ToDoList. De applicatie ToDoList heeft een class ToDoList. De class ToDoList heeft een instance variabele producten, wat een array is.
     Het arrayobject, bevat objecten van het type Product.
     De class product heeft de string naam en de int aantal als attributen. Maak de class product abstract.
-    Maak van product twee subclasses, namelijk BuyFood en Shopping. Laat in een demo-applicatie zien dat er BuyFood en Shopping in de ToDoList geplaatst kunnen worden. */
+    Maak van product twee subclasses, namelijk BuyFood en Shopping en implementeert voor ieder attribuut een waarde. Laat in een demo-applicatie zien dat er BuyFood en Shopping in de ToDoList geplaatst kunnen worden. */
 
         static void Main(string[] args)
         {
+        }
+    }
+
+    public class ToDoList
+    {
+
+    }
+
+    public abstract class Product
+    {
+        public string Naam;
+        public int Aantal;
+    }
+
+    public class BuyFood : Product
+    {
+        public void Supermarket(string Naam, int Aantal)
+        {
+            Naam = Melk;
+            Aantal = 2;
+        }
+    }
+
+    public class Shopping : Product
+    {
+        public void HudsonBay(string Naam, int Aantal)
+        {
+            Naam = Shoe;
+            Aantal = 1;
         }
     }
 }
