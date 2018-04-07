2cd8a37c40929dc4f55b1fa9138995c4d9846ca0
LSLIM <LSLIM@LS>
2018-04-07 12:24:33 +0200

    I put the instruction in the class

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 namespace ToDoList
 {
     class Program
-    {
+    { /* Instructies
+    Maak een applicatie ToDoList. De applicatie ToDoList heeft een class ToDoList. De class ToDoList heeft een instance variabele producten, wat een array is.
+    Het arrayobject, bevat objecten van het type Product.
+    De class product heeft de string naam en de int aantal als attributen. Maak de class product abstract.
+    Maak van product twee subclasses, namelijk BuyFood en Shopping. Laat in een demo-applicatie zien dat er BuyFood en Shopping in de ToDoList geplaatst kunnen worden. */
+
         static void Main(string[] args)
         {
         }
